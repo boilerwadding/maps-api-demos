e2fa1ff17e801bcfc056454e273d8e993abb555b
Ed Boiling <boilerwadding@users.noreply.github.com>
2015-02-26 15:31:33 +0000

    Added parks clip path demo
    Moved CoordinateHelper to an external JS to be shared between pages,
    added parks polygons in a new demo page

---
+++ spyglass/js/CoordinateHelper.js
@@ -0,0 +1,47 @@
+function CoordinateHelper(map) {
+  /**
+  * Offset the control container from the mouse by this amount.
+  */
+  this.ANCHOR_OFFSET_ = new google.maps.Point(8, 8);
+
+  /**
+  * Pointer to the HTML container.
+  */
+  this.node_ = this.createHtmlNode_();
+
+  // Add control to the map. Position is irrelevant.
+  map.controls[google.maps.ControlPosition.TOP].push(this.node_);
+
+
+  // Bind this OverlayView to the map so we can access MapCanvasProjection
+  // to convert LatLng to Point coordinates.
+  this.setMap(map);
+
+  // Register an MVC property to indicate whether this custom control
+  // is visible or hidden. Initially hide control until mouse is over map.
+  this.set('visible', false);
+}
+
+// Extend OverlayView so we can access MapCanvasProjection.
+CoordinateHelper.prototype = new google.maps.OverlayView();
+CoordinateHelper.prototype.draw = function() {};
+
+/**
+* @private
+* Helper function creates the HTML node which is the control container.
+* @return {HTMLDivElement}
+*/
+CoordinateHelper.prototype.createHtmlNode_ = function() {
+  var divNode = document.createElement('div');
+  divNode.id = 'latlng-control';
+  divNode.index = 100;
+  return divNode;
+};
+
+/* take a lat,long and just return the pixel position in the current view */
+CoordinateHelper.prototype.getPixelPosition = function(latLng) {
+  var projection = this.getProjection();
+  var point = projection.fromLatLngToContainerPixel(latLng);
+
+  return point;
+};
